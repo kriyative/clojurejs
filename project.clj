da4c01c85ddcc872604de9d023ff083d016f3627
Daniel Werner <daniel.d.werner@googlemail.com>
2011-01-17 21:53:34 +0100

    Use lein-difftest for tests.

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]]
   :dev-dependencies [[swank-clojure "1.2.1"]
-                     [lein-clojars "0.5.0"]]
+                     [lein-clojars "0.5.0"]
+                     [lein-difftest "1.3.1"]]
+  :hooks [leiningen.hooks.difftest]
   :test-path "src/clojurejs")
