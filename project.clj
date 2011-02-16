deeb6d1e63f4899497a80484704d5ab0f2e69adc
Ram Krishnan <rkris@kriyative.net>
2011-02-16 15:07:13 -0800

    Bumped up to release 1.2.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.kriyative/clojurejs "1.2.2"
+(defproject org.clojars.kriyative/clojurejs "1.2.3"
   :description "A naive Clojure to Javascript translator"
   :url "http://github.com/kriyative/clojurejs"
   :dependencies [[org.clojure/clojure "1.2.0"]
