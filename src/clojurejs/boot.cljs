46028d4d672cf233f1abdc3bca721e1f9fa206c9
Ram Krishnan <rkris@kriyative.net>
2011-02-24 17:32:31 -0800

    More concise definitions of `html` and `html-str`.

--- src/clojurejs/boot.cljs
+++ src/clojurejs/boot.cljs
@@ -106,22 +106,20 @@
   (dokeys [k attrs] (.setAttribute el k (get attrs k))))
 
 (defn html [spec]
-  (let [html1 (fn [spec]
-                (let [el (.createElement document (first spec))
-                      kindex 1]
-                  (when (map? (second spec))
-                    (html-set-attrs el (second spec))
-                    (set! kindex (+ 1 kindex)))
-                  (loop [i kindex]
-                    (when (< i (count spec))
-                      (.appendChild el (html (get spec i)))
-                      (recur (+ i 1))))
-                  el))]
-    (cond
-     (undefined? spec) (.createTextNode document "")
-     (string? spec) (.createTextNode document spec)
-     (array? spec) (html1 spec)
-     :else spec)))
+  (cond
+   (undefined? spec) (.createTextNode document "")
+   (string? spec) (.createTextNode document spec)
+   (array? spec) (let [el (.createElement document (first spec))
+                       kindex 1]
+                   (when (map? (second spec))
+                     (html-set-attrs el (second spec))
+                     (set! kindex (+ 1 kindex)))
+                   (loop [i kindex]
+                     (when (< i (count spec))
+                       (.appendChild el (html (get spec i)))
+                       (recur (+ i 1))))
+                   el)
+   :else spec))
 
 (defn html-str
   "Generate a string representation of the specified HTML spec."
@@ -129,18 +127,18 @@
   (let [map-str (fn [m]
                   (let [s []]
                     (dokeys [k m] (.push s (+ k "='" (get m k) "'")))
-                    (join " " s)))
-        html1 (fn [spec]
-                (join ""
-                      [(+ "<" (first spec)
-                          (if (map? (second spec)) (+ " " (map-str (second spec))) "")
-                          ">")
-                       (let [s []
-                             kindex (if (map? (second spec)) 2 1)]
-                         (loop [i kindex]
-                           (when (< i (count spec))
-                             (.push s (html-str (get spec i)))
-                             (recur (+ i 1))))
-                         (join "" s))
-                       (+ "</" (first spec) ">")]))]
-    (if (array? spec) (html1 spec) spec)))
+                    (join " " s)))]
+    (if (array? spec)
+      (join ""
+            [(+ "<" (first spec)
+                (if (map? (second spec)) (+ " " (map-str (second spec))) "")
+                ">")
+             (let [s []
+                   kindex (if (map? (second spec)) 2 1)]
+               (loop [i kindex]
+                 (when (< i (count spec))
+                   (.push s (html-str (get spec i)))
+                   (recur (+ i 1))))
+               (join "" s))
+             (+ "</" (first spec) ">")])
+      spec)))
